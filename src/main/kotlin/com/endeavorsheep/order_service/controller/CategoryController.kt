10cada98ca79e2ccd6bd631dbc43aa4223237e9a
higor <higorjugler@hotmail.com>
2024-06-26 18:07:52 -0300

    Persistir dados Db

--- src/main/kotlin/com/endeavorsheep/order_service/controller/CategoryController.kt
+++ src/main/kotlin/com/endeavorsheep/order_service/controller/CategoryController.kt
@@ -2,10 +2,13 @@ package com.endeavorsheep.order_service.controller
 
 import com.endeavorsheep.order_service.entity.Category
 import com.endeavorsheep.order_service.repository.RepositoryCategory
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.RestController
+import jakarta.validation.Valid
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.validation.FieldError
+import org.springframework.web.bind.MethodArgumentNotValidException
+import org.springframework.web.bind.annotation.*
+import kotlin.jvm.optionals.getOrNull
 
 @RestController
 @RequestMapping("/categories")
@@ -14,10 +17,42 @@ class CategoryController(
 ) {
 
     @GetMapping
-    fun list(@RequestParam name: String?): List<Category>{
+    fun list(@RequestParam name: String?): List<Category> {
         name?.let {
             return repositoryCategory.findByNameContainsIgnoreCase(it)
         }
         return repositoryCategory.findAll()
     }
+
+    @GetMapping("/{id}")
+    fun findById(@PathVariable id: Int): ResponseEntity<Category> {
+        val category = repositoryCategory.findById(id).getOrNull() ?: return ResponseEntity.notFound().build()
+        return ResponseEntity.ok(category)
+    }
+
+    @DeleteMapping("/{id}")
+    fun delete(@PathVariable id: Int): ResponseEntity<Unit> {
+        repositoryCategory.findById(id).getOrNull()?.let {
+            repositoryCategory.delete(it)
+        }
+        return ResponseEntity.noContent().build()
+    }
+
+    @PostMapping
+    fun save(@Valid @RequestBody category: Category): Category {
+        return repositoryCategory.save(category)
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException::class)
+    fun handleValidationExceptions(ex: MethodArgumentNotValidException): Map<String, String> {
+        val errors = mutableMapOf<String, String>()
+        ex.bindingResult.allErrors.forEach { error ->
+            val fieldName = (error as FieldError).field
+            val errorMessage = error.defaultMessage ?: ""
+            errors[fieldName] = errorMessage
+        }
+        return errors
+    }
+
 }
